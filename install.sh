e0b2e8bf6c68f544fb7672522feb900092a74a6d
Tino Nitze <tino.nitze@scout24.com>
2017-09-16 11:04:52 +0200

    Update install.sh
    Setting up RAM disk for Google Earth cache

--- install.sh
+++ install.sh
@@ -122,6 +122,14 @@ sudo apt-get -qq -f install > /dev/null
 sudo dpkg -i google-earth-stable*.deb > /dev/null 2>&1
 rm google-earth-stable*.deb
 
+echo "Setting up RAM disk for Google Earth cache"
+mkdir $HOME/.googleearth/Cache/
+sudo cp /etc/fstab /etc/fstab.bak
+echo "tmpfs     "$HOME"/.googleearth/Cache     tmpfs     rw,size=2G,x-gvfs-show     0 0" | sudo tee -a /etc/fstab
+sudo mount -a
+sudo chown lg  $HOME/.googleearth/Cache 
+
+
 # OS config tweaks (like disabling idling, hiding launcher bar, ...)
 echo "Setting system configuration..."
 sudo tee /etc/lightdm/lightdm.conf > /dev/null << EOM
